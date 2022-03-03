7bc0b29a5c6426010c7b4b8232761490ecbd0341
waidzk <waidzk30@gmail.com>
2022-03-03 14:28:06 +0700

    sampai memberikan event - react js bwa, desain product card sepatu 1st

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import './App.css';
+//Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+library.add(fas)
 
-const element = <Hello name="Dinda" />;
-function Hello(props) {
+function App() {
   return(
-    <h1>Hello, {props.name}</h1>
+    <div className="product-card">
+      <ProductDescription 
+      category="Running"
+      name="Nike SuperRep Go"
+      description="The Nike SuperRep Go combines comfortable foam cushioning, flexibility and support to get you moving in circuit based fitness classes" 
+      price="$69,97"
+      discountPrice="$50.00"/>
+      <ProductImage srcimg="/assets/shoe.jpg"/>
+    </div>
   )
 }
 
-function runElement() {
+function ProductDescription(props) {
+  const { category, name, description, price, discountPrice } = props;
   return(
-    element
-  )
+    <div className="product-description">
+        <span className="category">{category}</span>
+        <h1>{name}</h1>
+        <p>{description}</p>
+        <div className="rating">
+          <FontAwesomeIcon icon="fas fa-star" className="rating-icon"/>
+          <FontAwesomeIcon icon="fas fa-star" className="rating-icon"/>
+          <FontAwesomeIcon icon="fas fa-star" className="rating-icon"/>
+          <FontAwesomeIcon icon="fas fa-star" className="rating-icon"/>
+          <FontAwesomeIcon icon="fas fa-star" className="rating-icon"/>
+        </div>
+        <div className="price">
+          <h3>{price}</h3>
+          <h2>{discountPrice}</h2>
+        </div>
+        <div className="button-cart">
+          <button>
+            <FontAwesomeIcon icon="fas fa-shopping-cart" /> Add to cart
+          </button>
+        </div>
+      </div>
+  );
 }
 
-export default runElement;
+function ProductImage(props) {
+  const { srcimg } = props;
+  return (
+      <div className="product-image">
+        <img src={srcimg} alt="shoes"/>
+      </div>
+      )
+}
+
+export default App;
